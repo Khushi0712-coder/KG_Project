2f33f7186910cdef6af4ffac462ac071c5734aa1
KishtwarGold <khushi0712042@gmail.com>
2025-09-27 11:34:11 +0530

    pop

--- kishtwar-backend/server.js
+++ kishtwar-backend/server.js
@@ -5,6 +5,8 @@ import cors from "cors";
 import connectDB from "./config/db.js"; // MongoDB connection
 import userRoutes from "./routes/user.js";
 import paymentRoutes from "./routes/payment.js";
+import reviewRoutes from "./routes/review.js";
+import messageRoutes from "./routes/message.js"
 
 dotenv.config();
 
@@ -17,6 +19,8 @@ app.use(express.json());
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/payments", paymentRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/messages", messageRoutes);
 
 // Test route
 app.get("/", (req, res) => res.send("Backend running 🚀"));
